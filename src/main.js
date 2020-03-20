7b36a971c4c66d566606c491e02c5bb99e7f646b
maxoyed <maxoyed@gmail.com>
2020-03-20 19:02:08 +0800

    移除vue-router和vuex

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router";
-import store from "./store";
 import { Button } from "element-ui";
 import "fullpage.js/vendors/scrolloverflow";
 import VueFullpage from "vue-fullpage.js";
@@ -11,7 +9,5 @@ Vue.config.productionTip = false;
 Vue.use(Button).use(VueFullpage);
 
 new Vue({
-  router,
-  store,
   render: h => h(App)
 }).$mount("#app");
