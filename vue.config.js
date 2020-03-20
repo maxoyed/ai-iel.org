b93254961b25b371627ed0d3ffd46ddb487c77d9
maxoyed <maxoyed@gmail.com>
2020-03-20 17:36:54 +0800

    Update

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,7 @@ module.exports = {
       entry: "src/main.js",
       title: "人工智能创新精英联盟"
     }
-  }
+  },
+  outputDir: "dist",
+  publicPath: process.env.NODE_ENV === "production" ? "/ai-iel.org/" : "/"
 };
